e16d7cebbb42cc059fa407b0808e559a7688ad64
Cristian Suárez <cristiansuarezg7@gmail.com>
2020-09-06 13:36:56 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const getProductImg = async query => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(
+      `https://www.google.com/search?q=${query}&hl=en&sxsrf=ALeKk02bVoXgYpZoi0ufsHCm1g-7WFcBww:1599386737122&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjX_Mvyo9TrAhXjA2MBHZCaC54Q_AUoAXoECBwQAw&biw=1536&bih=722`,
+    );
+    await page.waitForSelector('.rg_i');
+
+    const img = await page.evaluate(() => {
+      return document.querySelector('.rg_i').src;
+    });
+
+    var source = await page.goto(img);
+
+    fs.writeFile(`./images/${query}.png`, await source.buffer(), err => {
+      if (err) return console.log(err);
+    });
+
+    await browser.close();
+
+    console.log('finished');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getProductImg('pepsi');
